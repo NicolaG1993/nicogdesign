3b63bd435da06fb5348cbf5beef9c11beba2c275
Nicola Gaioni <nicog.designer@gmail.com>
2022-02-09 18:07:38 +0100

    Projects improved, Contact improved

--- pages/contact.js
+++ pages/contact.js
@@ -16,6 +16,56 @@ export default function Contact() {
             <section className={styles[""]}>
                 <h1>CONTACT</h1>
             </section>
+            <section className={styles[""]}>
+                <h4>
+                    E-mail:{" "}
+                    <a href="mailto:nicog.designer@gmail.com">
+                        nicog.designer@gmail.com
+                    </a>
+                </h4>
+                <h4>
+                    Phone: <a href="tel:+4915737270232">+49 (0) 157 37270232</a>
+                </h4>
+                <h4>Berlin, Germany</h4>
+
+                <h2>SOCIALS</h2>
+                <h4>
+                    <a
+                        href="https://github.com/NicolaG1993"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        GitHub
+                    </a>
+                </h4>
+                <h4>
+                    <a
+                        href="https://stackoverflow.com/users/16337778/nicola-gaioni"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Stack Overflow
+                    </a>
+                </h4>
+                <h4>
+                    <a
+                        href="https://www.facebook.com/nicog.designer"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        FB
+                    </a>
+                </h4>
+                <h4>
+                    <a
+                        href="https://www.instagram.com/nik_gaioni/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        IG
+                    </a>
+                </h4>
+            </section>
         </div>
     );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -2,39 +2,6 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "../shared/styles/Projects.module.css";
 
-const projectLists = {
-    design: [
-        {
-            title: "NGD",
-            tags: ["Brand ID", "Graphic Design"],
-            description: "Lorem Ipsum",
-            link: "/projects",
-            external: null,
-        },
-    ],
-    coding: [
-        {
-            title: "da Mamy a Mamy",
-            tags: [
-                "E-Commerce",
-                "NextJS",
-                "Fullstack App",
-                "Website",
-                "Brand ID",
-            ],
-            description: "Lorem Ipsum",
-            link: "/projects",
-            external: "https://www.damamyamamy.com/",
-        },
-        {
-            title: "Bordogna Silvano Snc",
-            tags: ["NextJS", "Website", "Brand ID"],
-            description: "Lorem Ipsum",
-            link: "/projects",
-            external: "https://www.damamyamamy.com/",
-        },
-    ],
-};
 export default function Projects() {
     const [selection, setSelection] = useState("design");
 
@@ -74,23 +41,32 @@ export default function Projects() {
 
             <section className={styles["projects"]}>
                 {projectLists[selection].map((el) => (
-                    <div className={styles["project-box"]} key={el.title}>
-                        <div>
-                            <h2>{el.title}</h2>
-                        </div>
-                        <div>
-                            <h3>PICTURE HERE</h3>
-                        </div>
-                        <div>
-                            <h4>
-                                {el.tags.map((str) => (
-                                    <span key={str}>{str}</span>
-                                ))}
-                            </h4>
-                            <p>{el.description}</p>
+                    <div key={el.title}>
+                        <div className={styles["project-box"]}>
+                            <div>
+                                <div>
+                                    <h2>{el.title}</h2>
+                                    <h4>
+                                        {el.tags.map((str, i) =>
+                                            i === el.tags.length - 1 ? (
+                                                <span key={str}>{str}</span>
+                                            ) : (
+                                                <span key={str}>{str}, </span>
+                                            )
+                                        )}
+                                    </h4>
+                                </div>
 
-                            <button>{el.link}</button>
-                            {el.external && <button>{el.external}</button>}
+                                <div>
+                                    <h3>PICTURE HERE</h3>
+                                </div>
+                            </div>
+                            <div>
+                                <p>{el.description}</p>
+
+                                <button>{el.link}</button>
+                                {el.external && <button>{el.external}</button>}
+                            </div>
                         </div>
                     </div>
                 ))}
@@ -98,3 +74,41 @@ export default function Projects() {
         </div>
     );
 }
+
+// Potrei fare in modo che le due liste siano grandi ognuna 100vw e che slidano quando selezionate
+
+const projectLists = {
+    design: [
+        {
+            title: "NGD",
+            tags: ["Brand ID", "Graphic Design"],
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            link: "/projects",
+            external: null,
+        },
+    ],
+    coding: [
+        {
+            title: "da Mamy a Mamy",
+            tags: [
+                "E-Commerce",
+                "NextJS",
+                "Fullstack App",
+                "Website",
+                "Brand ID",
+            ],
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            link: "/projects",
+            external: "https://www.damamyamamy.com/",
+        },
+        {
+            title: "Bordogna Silvano Snc",
+            tags: ["NextJS", "Website", "Brand ID"],
+            description: "Lorem Ipsum",
+            link: "/projects",
+            external: "https://www.damamyamamy.com/",
+        },
+    ],
+};
